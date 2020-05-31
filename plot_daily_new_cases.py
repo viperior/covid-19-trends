be5875871d3951e89a6067bf7f2b9f85f13be0dd
viperior <viperior@gmail.com>
2020-05-31 16:47:57 +0000

    Update reference to week number label.

--- plot_daily_new_cases.py
+++ plot_daily_new_cases.py
@@ -2,5 +2,5 @@ import pandas as pd
 import plotly.express as px
 
 df = pd.read_json(path_or_buf = 'data/daily_texas.json')
-fig = px.bar(df, x = 'week_number_string', y = 'positiveIncrease')
+fig = px.bar(df, x = 'week_number_label', y = 'positiveIncrease')
 fig.write_html('sample/daily-new-cases-texas.html')

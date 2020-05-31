c0acf6da43f9481c6ab82aa1766e849ede3cda18
viperior <viperior@gmail.com>
2020-05-31 16:50:14 +0000

    Update plot HTML file name.

--- plot_new_cases.py
+++ plot_new_cases.py
@@ -3,4 +3,4 @@ import plotly.express as px
 
 df = pd.read_json(path_or_buf = 'data/daily_texas.json')
 fig = px.bar(df, x = 'week_number_label', y = 'positiveIncrease')
-fig.write_html('sample/daily-new-cases-texas.html')
+fig.write_html('sample/weekly-new-cases-texas.html')

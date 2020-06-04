b1c4c2de8c584918c15ea718c89ea756140e2d88
viperior <viperior@gmail.com>
2020-06-04 13:00:15 +0000

    Add step to create the data subdirectory if it does not already exist.

--- fetch_historical_state_data.py
+++ fetch_historical_state_data.py
@@ -1,8 +1,10 @@
 import json
+import os
 import requests
 
 api_endpoint_url = 'https://covidtracking.com/api/v1/states/daily.json'
-target_data_file_path = 'data/daily.json'
+target_data_directory = 'data'
+target_data_file_path = target_data_directory + '/daily.json'
 
 print(api_endpoint_url)
 r = requests.get(api_endpoint_url)
@@ -10,6 +12,9 @@ print(r.status_code)
 print(r.headers['content-type'])
 print(r.encoding)
 print('Saving data to ' + target_data_file_path + '...')
+
+if not os.path.exists(target_data_directory):
+    os.makedirs(target_data_directory)
 
 with open(target_data_file_path, 'w') as target_data_file:
     json.dump(r.json(), target_data_file)

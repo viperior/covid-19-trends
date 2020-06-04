5d6ec3a156f2748d781996c46040079e3ff9c88e
viperior <viperior@gmail.com>
2020-06-04 12:46:12 +0000

    Improving status messages in data reload module.

--- reload_data.py
+++ reload_data.py
@@ -3,15 +3,23 @@
 print('Beginning full reload of COVID-19 case data...')
 
 # Retrieve the latest case data.
+print('Retrieving data from COVID-19 tracking project...')
 import fetch_historical_state_data
+print('Latest case data retrieved!')
 
 # Refresh the list of states to plot data for.
+print('Updating the list of dates to plot...')
 import create_states_to_plot_json
+print('State list updated!')
 
 # Extract and transform data.
+print('Transforming data and adding features (go pandas, go!)...')
 import extract_daily_new_cases
+print('Data is ready!')
 
 # Refresh charts.
+print('Plotting data charts using plotly...')
 import plot_new_cases
+print('Charts updated!')
 
-print('Data reload complete! 🌟')
+print('Data reload complete!')

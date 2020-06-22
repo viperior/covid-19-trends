7bc2de8d2f1b85e3b48a4b317cab47b9533c0acd
viperior <viperior@gmail.com>
2020-06-22 00:54:40 +0000

    Sort JSON data in chronological order by date.

--- fetch_historical_state_data.py
+++ fetch_historical_state_data.py
@@ -2,6 +2,14 @@ import json
 import os
 import requests
 
+def extract_date(json):
+    try:
+        # Also convert to int since update_time will be string. When comparing
+        # strings, "10" is smaller than "2".
+        return int(json['date'])
+    except KeyError:
+        return 0
+
 api_endpoint_url = 'https://covidtracking.com/api/v1/states/daily.json'
 target_data_directory = 'data'
 target_data_file_path = target_data_directory + '/daily.json'
@@ -18,5 +26,14 @@ if not os.path.exists(target_data_directory):
 
 with open(target_data_file_path, 'w') as target_data_file:
     json.dump(r.json(), target_data_file)
+    
+# Reload and sort the json file chronologically by date.
+with open(target_data_file_path, 'r') as json_file:
+    json_data = json.load(json_file)
+    
+json_data.sort(key=extract_date)
+
+with open(target_data_file_path, 'w') as output_file:
+    json.dump(json_data, output_file)
     
 print('Data extraction complete.')

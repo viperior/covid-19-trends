10e86e4f6fbdc4a5276d0081b978e176023d82ef
viperior <viperior@gmail.com>
2020-06-22 14:30:49 +0000

    Correct reference to charts subdirectory.

--- reload_and_commit_data.py
+++ reload_and_commit_data.py
@@ -3,20 +3,26 @@ import subprocess
 print('Reloading data...')
 import reload_data
 print('Reloaded data!')
+
 print('Getting repo status...')
 subprocess.call(["git", "status"])
+
 print('Adding updated data files...')
 subprocess.call(["git", "add", "data/"])
 print('Data files added!')
+
 print('Adding updated chart files...')
-subprocess.call(["git", "add", "charts/"])
+subprocess.call(["git", "add", "docs/charts/"])
 print('Chart files added!')
+
 print('Adding commit message...')
 subprocess.call(["git", "commit", "-m", 'Reload data.'])
 print('Commit message added!')
+
 print('Pushing changes...')
 subprocess.call(["git", "push"])
 print('Changes successfully pushed!')
+
 print('Verifying repo status...')
 subprocess.call(["git", "status"])
 print('Repo status verified!')
